960f8e61f4867f3b2c012d0ecca0f4a89ba11516
Mateusz Luszczewski <mateusz.luszczewski@grupawp.pl>
2019-11-09 16:45:14 +0100

    Flask

--- app.py
+++ app.py
@@ -2,14 +2,6 @@ import sqlite3
 from flask import Flask, render_template, request, g
 app = Flask(__name__)
 
-DATABASE = "database.db"
-
-def get_db():
-    db = getattr(g, 'database', None)
-    if db is None:
-        db = g.database = sqlite3.connect(DATABASE)
-    return db
-
 @app.route('/list')
 
 def list():
@@ -24,14 +16,6 @@ def list():
     con.close()
 
     return render_template("list.html", rows = rows)
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, 'database', None)
-    if db is not None:
-        db.Close()
-
 
 if __name__ == '__main__':
    app.run(debug = True)
