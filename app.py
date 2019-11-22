f4419577362028a86da71718776e152717f6c2d6
Mateusz Luszczewski <mateusz.luszczewski@grupawp.pl>
2019-11-22 12:58:12 +0100

    usuniecie /list

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import sqlite3
 from flask import Flask, render_template, request, g
 app = Flask(__name__)
 
-@app.route('/list')
+@app.route('/')
 
 def list():
     con = sqlite3.connect('database.db')
     cur = con.cursor()
     con.row_factory = sqlite3.Row
 
-    cur.execute("SELECT * FROM offers limit 2")
+    cur.execute("SELECT * FROM offers limit 5")
 
     rows = cur.fetchall()
     con.commit()
     con.close()
 
-    return render_template("list.html", rows = rows)
+    return render_template("list.html", rows=rows)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
