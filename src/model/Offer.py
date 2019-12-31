ad8c02f04e9b44f30a8ff787766d69608b068a49
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2019-12-31 09:49:12 +0100

    Offer model reshaping

--- src/model/Offer.py
+++ src/model/Offer.py
@@ -1,32 +1,99 @@
+from src.model.Const import CATEGORIES, CURRENCIES
+from re import sub
+
+
 class Offer:
 
-    def __init__(self,
-                title,
-                company,
-                city,
-                offerurl,
-                salary='',
-                techstack=[],
-                experience=[],
-                logourl=''):
-            
-                self.title = title
-                self.company = company
-                self.city = city
-                self.salary = salary
-                self.techstack = techstack
-                self.experience = experience
-                self.logourl = logourl
-                self.offerurl = offerurl
+    def __init__(
+        self,
+        title,
+        company,
+        city,
+        offerurl,
+        salary,
+        techstack,
+        experience
+    ):
+
+        self.title = title
+        self.company = company
+        self.city = city
+        self.offerurl = offerurl
+        self.salary = salary
+        self.techstack = techstack
+        self.experience = experience
+        self.category = self._classify()
 
     def __repr__(self):
-        return ('Offer:\n'
-                f'Title: {self.title}\n'
-                f'Company: {self.company}\n'
-                f'City: {self.city}\n'
-                f'Salary: {self.salary}\n'
-                f'TechStack: {self.techstack}\n'
-                f'Experience: {self.experience}\n'
-                f'LogoURL: {self.logourl}\n'
-                f'OfferURL: {self.offerurl}\n')
-                
+        return str(self.__dict__)
+
+    @property
+    def experience(self):
+        return self._experience
+
+    @experience.setter
+    def experience(self, value):
+        self._experience = []
+        if type(value) is list:
+            self._experience = value
+        if type(value) is str:
+            if value.find(',') == -1:
+                self._experience = value.split(',')
+            self._experience = [value.title()]
+
+    @property
+    def techstack(self):
+        return self._techstack
+
+    @techstack.setter
+    def techstack(self, value):
+        self._techstack = []
+        if type(value) is list:
+            self._techstack = value
+        if type(value) is str and value != '':
+            if value.find(',') == -1:
+                self._techstack = value.split(',')
+            self._techstack = [value]
+
+    @property
+    def offerurl(self):
+        return self._offerurl
+
+    @offerurl.setter
+    def offerurl(self, url):
+        if url is not None and url != '':
+            self._offerurl = url
+        else:
+            raise ValueError(f'Offer URL {url} cannot be empty or None.')
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, value):
+        self._salary = {'floor': None, 'ceiling': None, 'currency': None}
+        if value is not None and value != '':
+            currency = sub(r'[^a-zA-Z]', '', value)
+            split = value.split('-')
+            if currency.lower() in CURRENCIES:
+                self._salary['currency'] = currency.upper()
+            if len(split) == 2:
+                floor = sub(r'[^0-9]', '', split[0])
+                ceiling = sub(r'[^0-9]', '', split[1])
+                if floor.isdecimal() and ceiling.isdecimal():
+                    self._salary['floor'] = floor
+                    self._salary['ceiling'] = ceiling
+
+    def _classify(self):
+        description = sub(r'-', '', self.title).lower()
+        description = sub(r'[^a-zA-Z.+# ]', ' ', description).lower()
+        for keyword in description.split():
+            for category, tags in CATEGORIES.items():
+                if keyword in tags:
+                    return category
+        for tech in self.techstack:
+            for category, tags in CATEGORIES.items():
+                if tech.lower() in tags:
+                    return category
+        return 'Other'
