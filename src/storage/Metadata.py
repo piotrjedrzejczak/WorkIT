08c5cad02ae73aaafa14753e11ede2a3841eaab8
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2019-12-31 09:50:42 +0100

    pep8 compliance

--- src/storage/Metadata.py
+++ src/storage/Metadata.py
@@ -1,10 +1,11 @@
 import sqlite3
 from os import remove
 
+
 class Storage():
 
     def Metadata(self, offers):
-        
+
         remove('./database.db')
 
         connection = sqlite3.connect('database.db')
@@ -19,9 +20,9 @@ class Storage():
             tuple([str(field) for field in offer.__dict__.values()])
             for offer in offers
         ]
-        
+
         template = """INSERT INTO Offers VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
         cursor.executemany(template, queries)
-        
+
         connection.commit()
         connection.close()
