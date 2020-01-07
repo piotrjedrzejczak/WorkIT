62eaa78cbdb2d7cd15dcf4294f5447b7c37635f5
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2020-01-07 01:37:20 +0100

    placeholders

--- workit/__init__.py
+++ workit/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__, static_url_path="/static")
-app.config["MONGO_URI"] = "mongodb+srv://dev:workit2020@workitcluster-nege2.mongodb.net/WorkIT?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@workitcluster-nege2.mongodb.net/WorkIT?retryWrites=true&w=majority" # noqa E501
 app.config["MONGO_DBNAME"] = "WorkIT"
 app.config["SECRET_KEY"] = "VerySecretKey"
 mongo = PyMongo(app)
 collection = mongo.db["Offers"]
 
-from workit import routes
+from workit import routes  # noqa E402
