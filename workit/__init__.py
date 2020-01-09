99fcab0e1e0b3b4be7de475c2729d4dcd70be8f8
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2020-01-09 01:33:01 +0100

    Path Fix

--- workit/__init__.py
+++ workit/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 
-app = Flask(__name__, static_url_path="/static")
-app.config["MONGO_URI"] = "mongodb+srv://<username>:<password>@workitcluster-nege2.mongodb.net/WorkIT?retryWrites=true&w=majority" # noqa E501
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://dev:workit2020@workitcluster-nege2.mongodb.net/WorkIT?retryWrites=true&w=majority" # noqa E501
 app.config["MONGO_DBNAME"] = "WorkIT"
 app.config["SECRET_KEY"] = "VerySecretKey"
 mongo = PyMongo(app)
