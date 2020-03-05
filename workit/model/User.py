17e88594e2f74e696c3e23850eedefc70a8d84c4
Mateusz Luszczewski <mateusz.luszczewski@grupawp.pl>
2020-03-05 12:53:18 +0100

    added retrun on set password

--- workit/model/User.py
+++ workit/model/User.py
@@ -18,6 +18,7 @@ class User(UserMixin):
 
     def set_password(self, password):
         self.password = generate_password_hash(password, method='sha256')
+        return self.password
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
