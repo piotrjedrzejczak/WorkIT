945716167d4f1338f4725d1f6ead77cf3b631525
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2020-03-07 10:11:47 +0100

    index required for keyword search

--- workit/__init__.py
+++ workit/__init__.py
@@ -10,11 +10,14 @@ login_manager.init_app(app)
 app.config["SECRET_KEY"] = "VerySecretKey"
 client = MongoClient(environ['DB_PORT_27017_TCP_ADDR'], 27017)
 collection = client.db.offers
+collection.create_index(
+    [('title', 'text'), ('techstack', 'text')],
+    default_language='english'
+)
 users_collection = client.db.users
 
 with app.app_context():
     from . import routes
     from . import auth
-
 
 from workit import routes  # noqa E402
